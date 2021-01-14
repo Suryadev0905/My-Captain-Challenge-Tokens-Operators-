ee90f313c4f63fcebd25405b28e6409510fa76aa
Suryadev0905 <67860643+Suryadev0905@users.noreply.github.com>
2021-01-14 22:09:58 +0530

    Add files via upload

---
+++ Mycaptainproduct.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+
+    cout<<"Enter 1st Number : "<<endl;
+    cin>>a;
+    cout<<"Enter 2nd Number : "<<endl;
+    cin>>b;
+    c = a*b;
+    cout<<"Product : "<<c<<endl;
+
+    return 0;
+}
